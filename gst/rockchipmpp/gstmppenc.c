d88081e5b724a44c25720f4c753fc945e19bcb64
zzx <zzx@xzhtek.com>
2022-05-20 11:18:22 +0800

    38#用qplayer播放视频报错：弹出错误提示框，视频无法播放

--- gst/rockchipmpp/gstmppenc.c
+++ gst/rockchipmpp/gstmppenc.c
@@ -622,6 +622,7 @@ gst_mpp_enc_set_format (GstVideoEncoder * encoder, GstVideoCodecState * state)
     GST_INFO_OBJECT (self, "converting to aligned NV12");
   }
 
+  mpp_frame_set_fmt (self->mpp_frame, format);
   mpp_frame_set_width (self->mpp_frame, width);
   mpp_frame_set_height (self->mpp_frame, height);
   mpp_frame_set_hor_stride (self->mpp_frame, GST_MPP_VIDEO_INFO_HSTRIDE (info));
